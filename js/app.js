c9c9e569cff2a9c3807c860a6da1d4ff3fe9d858
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-28 12:34:19 +0200

    Lors de la saisie de text dans l'input "#setText", faire apparaitre le text dans la div '#needText'

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-
+//Supprimer la div "#remove" au click sur le boutton '#removeDiv';
 
     $("#removeDiv").click( function(){
     	$("#remove").remove();
-
 });
 
+
+// Lors de la saisie de text dans l'input "#setText", faire apparaitre le text dans la div '#needText'; (on-change);
+$('#setText').change( function(){
+	$('#needText').append( $('#setText').val() );
+});
